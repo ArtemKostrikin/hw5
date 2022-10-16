cf0da77f671dfa98765c201379c4a97abddc54fc
Artem <artemkostrikin12@gmail.com>
2022-10-16 16:22:18 +0300

    36 задача

--- Program.cs
+++ Program.cs
@@ -1,34 +1,30 @@
-﻿using static System.Console;
-WriteLine("Введите размер массива");
-int size = Convert.ToInt32(ReadLine());
-int[] numbers =new int[size];
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
-WriteLine("Вот ваш массив:");
+Console.WriteLine("Вот наш массив: ");
 PrintArray(numbers);
-int count = 0;
+int sum = 0;
 
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
-count++;
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
 
-Write($"всего {numbers.Length} чисел, {count} из них чётные");
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
 
-void FillArrayRandomNumbers(int [] numbers)
+void FillArrayRandomNumbers(int[] numbers)
 {
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        numbers[i] = new Random().Next(100,1000);
-    }
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
 }
-void PrintArray(int [] numbers)
+void PrintArray(int[] numbers)
 {
-    Write("[");
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        Write(numbers[i] + " ");
-    }
-    Write("]");
-    WriteLine();
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
 }
-
-
